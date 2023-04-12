023421688099d61db61882ec14696849e0232470
Andriikryl <andreyk557@gmail.com>
2023-04-12 16:34:44 +0300

    feat[transition]

--- js/app.js
+++ js/app.js
@@ -9,8 +9,19 @@ accordion.addEventListener("click", (e) => {
 
 function toggleAcordian(panelToActive) {
   const buttons = panelToActive.parentElement.querySelectorAll("button");
+  const contents =
+    panelToActive.parentElement.querySelectorAll(".accordion-content");
   console.log(buttons);
   buttons.forEach((button) => {
     button.setAttribute("aria-expanded", false);
   });
+
+  contents.forEach((content) => {
+    content.setAttribute("aria-hidden", true);
+  });
+
+  panelToActive.querySelector("button").setAttribute("aria-expanded", true);
+  panelToActive
+    .querySelector(".accordion-content")
+    .setAttribute("aria-hidden", false);
 }
