8030c27890f0d4c6f507eba6eed2e295f8872618
Andriikryl <andreyk557@gmail.com>
2023-04-12 16:18:01 +0300

    feat[toggleAcordian-function]

--- js/app.js
+++ js/app.js
@@ -3,4 +3,14 @@ const accordion = document.querySelector(".accordion");
 accordion.addEventListener("click", (e) => {
   const activePanel = e.target.closest(".accordion-panel");
   if (!activePanel) return;
+  console.log(activePanel);
+  toggleAcordian(activePanel);
 });
+
+function toggleAcordian(panelToActive) {
+  const buttons = panelToActive.parentElement.querySelectorAll("button");
+  console.log(buttons);
+  buttons.forEach((button) => {
+    button.setAttribute("aria-expanded", false);
+  });
+}
